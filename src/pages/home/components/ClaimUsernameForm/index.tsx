995a1c38c1e48bbf587fdc4fc41ccaeea691e976
Gabriel Pires <gabpiresdev@gmail.com>
2023-02-09 19:19:44 -0300

    redirecting from claim username form to register with username query param

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Text, TextInput } from '@ignite-ui/react'
+import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -18,16 +19,20 @@ const claimUsernameFormSchema = z.object({
 type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export const ClaimUsernameForm = () => {
+  const router = useRouter()
+
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<ClaimUsernameFormData>({
     resolver: zodResolver(claimUsernameFormSchema),
   })
 
   const handleClaimUsername = async (data: ClaimUsernameFormData) => {
-    console.log(data)
+    const { username } = data
+
+    await router.push(`/register?username=${username}`)
   }
 
   return (
@@ -39,7 +44,7 @@ export const ClaimUsernameForm = () => {
           placeholder={'seu-usuario'}
           {...register('username')}
         />
-        <Button size={'sm'} type={'submit'}>
+        <Button size={'sm'} type={'submit'} disabled={isSubmitting}>
           Reservar
           <ArrowRight />
         </Button>
