24777c032e3dbad9b565b17edf7fea3a95e965f1
Gabriel Pires <gabpiresdev@gmail.com>
2023-03-07 19:38:14 -0300

    using react query to fetch availability data

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,5 +1,7 @@
 import './../lib/dayjs'
 
+import { queryClient } from '@/lib/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
@@ -11,8 +13,10 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
